6939d3c414ace87116f160a5e8b013db2460eaf2
Nor <ruiz.norberto@comunidad.unam.mx>
2026-06-01 21:19:06 -0600

    Punto de reparacion 01/06/2026

--- TaskMaster/app/build.gradle.kts
+++ TaskMaster/app/build.gradle.kts
@@ -70,6 +70,14 @@ dependencies {
     // MPAndroidChart
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
+    // Gson
+    implementation("com.google.code.gson:gson:2.11.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
